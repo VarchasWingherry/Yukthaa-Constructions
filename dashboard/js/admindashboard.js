37924736145fba13bd980090f04dec0ba212706f
VarchasWingherry <varchas.wingherry@gmail.com>
2025-03-28 18:41:15 +0530

    code

--- dashboard/js/admindashboard.js
+++ dashboard/js/admindashboard.js
@@ -1,32 +1,18 @@
 const body = document.querySelector("body"),
-      modeToggle = body.querySelector(".mode-toggle");
-      sidebar = body.querySelector("nav");
-      sidebarToggle = body.querySelector(".sidebar-toggle");
-
-let getMode = localStorage.getItem("mode");
-if(getMode && getMode ==="dark"){
-    body.classList.toggle("dark");
-}
+      sidebar = document.querySelector(".nav-dash"),
+      sidebarToggle = document.querySelector(".sidebar-toggle");
 
 let getStatus = localStorage.getItem("status");
-if(getStatus && getStatus ==="close"){
-    sidebar.classList.toggle("close");
+if (getStatus && getStatus === "close") {
+    sidebar.classList.add("close");
 }
-
-modeToggle.addEventListener("click", () =>{
-    body.classList.toggle("dark");
-    if(body.classList.contains("dark")){
-        localStorage.setItem("mode", "dark");
-    }else{
-        localStorage.setItem("mode", "light");
-    }
-});
 
 sidebarToggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
-    if(sidebar.classList.contains("close")){
+
+    if (sidebar.classList.contains("close")) {
         localStorage.setItem("status", "close");
-    }else{
+    } else {
         localStorage.setItem("status", "open");
     }
-})
+});
